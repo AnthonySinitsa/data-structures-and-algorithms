85cad2d974f222b34ba7b97ece4f926556b0402a
AnthonySinitsa <itonyemail@gmail.com>
2023-05-22 16:53:30 +0000

    did challenge 5

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -117,6 +117,18 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 
 const salesData = (hours, data) => {
   // Solution code here...
+  let formattedData = [];
+
+  hours.forEach((hour, index) => {
+    let cookiesSold = data[index];
+    let salesObject = {
+      sales: `${cookiesSold} cookies`,
+      time: hour
+    };
+    formattedData.push(salesObject);
+  });
+
+  return formattedData;
 };
 
 /* ------------------------------------------------------------------------------------------------
