63ead98f3c42d3cfdb567e12b870f947ccde8243
AnthonySinitsa <itonyemail@gmail.com>
2023-06-23 22:55:11 +0000

    finished stack and queueue

--- python/tests/data_structures/test_queue.py
+++ python/tests/data_structures/test_queue.py
@@ -111,3 +111,49 @@ def test_exhausted():
     actual = q.is_empty()
     expected = True
     assert actual == expected
+
+def test_can_enqueue_into_queue():
+    queue = Queue()
+    queue.enqueue(1)
+    assert queue.front.value == 1
+
+def test_can_enqueue_multiple_values_into_queue():
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    assert queue.front.value == 1
+    assert queue.rear.value == 2
+
+def test_can_dequeue_out_of_a_queue_the_expected_value():
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    value = queue.dequeue()
+    assert value == 1
+    assert queue.front.value == 2
+
+def test_can_peek_into_a_queue_seeing_the_expected_value():
+    queue = Queue()
+    queue.enqueue(1)
+    value = queue.peek()
+    assert value == 1
+
+def test_can_successfully_empty_a_queue_after_multiple_dequeues():
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.dequeue()
+    queue.dequeue()
+    assert queue.is_empty()
+
+def test_can_successfully_instantiate_an_empty_queue():
+    queue = Queue()
+    assert queue.is_empty()
+
+def test_calling_dequeue_or_peek_on_empty_queue_raises_exception():
+    queue = Queue()
+    with pytest.raises(InvalidOperationError):
+        queue.dequeue()
+
+    with pytest.raises(InvalidOperationError):
+        queue.peek()
