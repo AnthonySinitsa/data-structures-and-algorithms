26a8eee2473276f749170422fe65e06009862d8e
AnthonySinitsa <itonyemail@gmail.com>
2023-06-28 21:50:34 +0000

    finisehd

---
+++ python/code_challenges/code_challenges/stackQueueBrackets/README.md
@@ -0,0 +1,20 @@
+# Challenge Title
+
+stack_queue_brackets
+
+## Approach & Efficiency
+
+time complexity: O(n)
+space complexity: O(n)
+
+## contribution
+
+jared ciccarelo, Logan reese, sarah glass, slava makeev
+
+## Solution
+
+run  pytest tests/code_challenges/test_stack_queue_brackets.py  from root
+
+## whitebaord
+
+![stackQueueBrackets](/code_challenges/code_challenges/stackQueueBrackets/1.jpg)
--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,28 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
+def multi_bracket_validation(str):
+  #create instance of stack used for storing brackets
+  stack = Stack()
+  left_brackets = ['(', '[', '{']
+  right_brackets = [')', ']', '}']
 
-def multi_bracket_validation():
-    pass
+  for i in str:
+    if i in left_brackets:
+      stack.push(i)
+    elif i in right_brackets:
+      if stack.is_empty():
+        return False
+      #check if the right bracket matches the left bracket
+      if i == ')' and stack.peek() == '(':
+      #if it does, pop the left bracket off the stack
+        stack.pop()
+      elif i == ']' and stack.peek() == '[':
+        stack.pop()
+      elif i == '}' and stack.peek() == '{':
+        stack.pop()
+      else:
+        return False
+  if stack.is_empty():
+    return True
+  else:
+    return False
